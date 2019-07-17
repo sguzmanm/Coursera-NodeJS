6b1f5c5a0c537cfd8dc5482c86b13a56c6e2628f
Sergio Guzmán Mayorga <s.guzmanm98@gmail.com>
2019-07-17 14:07:36 -0500

    Simple NOde Module

---
+++ NodeExamples/index.js
@@ -0,0 +1,19 @@
+var rect = {
+  perimeter: (x, y) => 2 * (x + y),
+  area: (x, y) => x * y
+};
+
+function solveRect(l, w) {
+  console.log("Length " + l + " width " + w);
+  if (l <= 0 || w <= 0) {
+    console.log("Fuck you");
+  } else {
+    console.log(
+      "Permiter " + rect.perimeter(l, w) + ", area " + rect.area(l, w)
+    );
+  }
+}
+
+solveRect(1, 2);
+solveRect(0, 2);
+solveRect(9, 9);
