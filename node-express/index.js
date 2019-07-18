26b905f9fff9e0f0b59e3a1fb0d97e0cd86cf07c
Sergio Guzmán Mayorga <s.guzmanm98@gmail.com>
2019-07-17 20:14:58 -0500

    Dish router

--- node-express/index.js
+++ node-express/index.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const http = require("http");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
 const hostName = "localhost";
 const port = "3000";
 
+const dishRouter = require("./routes/dishRouter");
 const app = express();
 
 app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use("/dishes", dishRouter);
 app.use(express.static(__dirname + "/public"));
+
 app.use((req, res, next) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
---
+++ node-express/routes/dishRouter.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const dishRouter = express.Router();
+// Routes for /
+dishRouter
+  .route("/")
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    next();
+  })
+  .get((req, res, next) => {
+    res.end("Will send all the dishes to you");
+  })
+  .post((req, res, next) => {
+    res.end("Will add the dish " + req.body.name + " " + req.body.description);
+  })
+  .put((req, res, next) => {
+    res.statusCode = 403;
+    res.end("PUT op not supported on dishes");
+  })
+  .delete((req, res, next) => {
+    res.statusCode = 403;
+    res.end("PUT op not supported on dishes");
+  });
+//Routes for /:dishId
+dishRouter
+  .route("/:dishId")
+  .get((req, res, next) => {
+    res.end(`Will send details of ${req.params.dishId} to you`);
+  })
+  .post((req, res, next) => {
+    res.statusCode = 403;
+    res.end("PUT op not supported on dishes&" + req.params.dishId);
+  })
+  .put((req, res, next) => {
+    res.write(`Updating ${req.params.dishId}`);
+    res.end(
+      `Will update the dish ${req.body.name} with details ${
+        req.body.description
+      }`
+    );
+  })
+  .delete((req, res, next) => {
+    res.write(`Deleting ${req.params.dishId}`);
+  });
+
+module.exports = dishRouter;
