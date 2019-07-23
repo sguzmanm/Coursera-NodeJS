c0d341112cb2ec3f17ea8dbdbe4dcc7ec59c4714
Sergio <sguzman@truora.com>
2019-07-22 21:38:30 -0500

    Update assignment 1

--- node-express/routes/dishRouter.js
+++ node-express/routes/dishRouter.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const dishRouter = express.Router();
+
+dishRouter.use(bodyParser.json());
 // Routes for /
 dishRouter
   .route("/")
